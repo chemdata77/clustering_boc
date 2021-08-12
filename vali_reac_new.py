455ef45e461c2c614085e253c97be482a9ee03ff
zhihuiqin <qzh@bistu.edu.cn>
2021-08-12 19:11:49 +0800

    kmeans reac_boc

--- vali_reac_new.py
+++ vali_reac_new.py
@@ -7,10 +7,10 @@ seed = 1234
 random.seed(seed)
 # np.random.seed(seed)
 
-with open('reactions.txt', 'rb') as fp:
+with open('./data/reactions.txt', 'rb') as fp:
     b = pickle.load(fp)
-print(b[81040095])
-print(len(b)) # this should be same with the output in all.log
+#print(b[81040095])
+#print(len(b)) # this should be same with the output in all.log
 
 random.shuffle(b)
 # one_cnt = 0
@@ -19,18 +19,18 @@ random.shuffle(b)
 #         one_cnt += 1
 # print(one_cnt)
 
-with open('qm9_id_boc_mini.lst', 'rb') as fp:
+with open('./data/kmeans/qm9_id_boc_mini.lst', 'rb') as fp:
     c = pickle.load(fp)
 
-print(c[100000])
-print(len(c))
+#print(c[100000])
+#print(len(c))
 
 
 i = 0
-for idx in range(100):
+for idx in range(20):
     ret = []
-    pos_cnt = 500000
-    neg_cnt = 500000
+    pos_cnt = 2500000
+    neg_cnt = 2500000
     while((pos_cnt > 0 or neg_cnt > 0) and i < len(b)):
         # if(abs(b[i][4]) > 0.05):
         #     i += 1
@@ -53,8 +53,8 @@ for idx in range(100):
             pass
         if pos_cnt == 0 and neg_cnt == 0:
             random.shuffle(ret)
-            os.system('rm -rf ' + 'dataset_new_' + str(idx+1) + '.hdf5')
-            f = h5py.File('dataset_new_' + str(idx+1) + '.hdf5', 'w')
+            os.system('rm -rf ' + './data/kmeans/dataset_new_' + str(idx+1) + '.hdf5')
+            f = h5py.File('./data/kmeans/dataset_new_' + str(idx+1) + '.hdf5', 'w')
             f.create_group('/grp1') # or f.create_group('grp1')
             f.create_dataset('dset1', compression='gzip', data=np.array(ret)) # or f.create_dataset('/dset1', data=data)
             f.close()
@@ -63,5 +63,5 @@ for idx in range(100):
             ret = []
         i += 1
 
-print('total reaction count: ', i)
+#print('total reaction count: ', i)
 print('Done.')
