e566dbacf8ba8c526a3d61a5beefe8e670007dea
zhihuiqin <qzh@bistu.edu.cn>
2021-08-16 22:20:02 +0800

    argv model

--- train_model.py
+++ train_model.py
@@ -5,24 +5,15 @@ from sklearn.metrics import accuracy_score
 from sklearn.linear_model import SGDClassifier
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn import svm
-from sklearn import tree
-from sklearn.svm import LinearSVC
+import argparse 
 seed = 1234
 random.seed(seed)
 np.random.seed(seed)
 
-file_cnt = 5
-step_cnt = 1e6
+def load_data():
+    global cur_file_id, BoC_size, data_arr, label_arr
 
-BoC_size = 0
-
-dataset = None
-data_arr = None
-label_arr = None
-for i in range(1, file_cnt+1):
-    f = h5py.File('./data/dataset_new_' + str(i) + '.hdf5', 'r')
+    f = h5py.File('./data/dataset_new_' + str(cur_file_id) + '.hdf5', 'r')
     print('start to load data.')
     dataset = f['dset1'][:]
     print(len(dataset),len(dataset[0]))
@@ -41,40 +32,66 @@ for i in range(1, file_cnt+1):
         label.append(int(dataset[j][-1]))
     print(zero_cnt, 'start to cat data')
    
-    if i == 1:
+    if cur_file_id == 1:
         print('input BoC length is: ', len(dataset[0])-1)
         BoC_size = len(dataset[0])-1
-        data_arr = np.zeros((int(file_cnt * step_cnt), BoC_size))
-        label_arr = np.zeros(int(file_cnt * step_cnt), dtype=np.int)
+        data_arr = np.array(data, dtype=np.float32)
+        label_arr = np.array(label)
     else:
-        pass
-    data_arr[int((i-1)*step_cnt):int(i*step_cnt)] = np.array(data, dtype=np.float32)
-    label_arr[int((i-1)*step_cnt):int(i*step_cnt)] = np.array(label)     
-
-    dataset = None
-    data = None
-    label = None
-    print(i, data_arr.shape, label_arr.shape)
+        data_arr = np.concatenate((data_arr, np.array(data, dtype=np.float32)), axis=0)
+        label_arr = np.concatenate((label_arr, np.array(label)), axis=0)
+    
+    print(cur_file_id, data_arr.shape, label_arr.shape)
+    cur_file_id += 1
     f.close()
 
-train_sets = data_arr[:int(len(data_arr)*0.9)]
-test_sets = data_arr[int(len(data_arr)*0.9):]
-train_labels = label_arr[:int(len(data_arr)*0.9)]
-test_labels = label_arr[int(len(data_arr)*0.9):]
+def create_model(s):
+    print('初始化'+s+'模型')
+    if s == 'SDG':
+        clf = make_pipeline(StandardScaler(), SGDClassifier(max_iter=1000, tol=1e-3))
+    elif s == 'KNN':
+        clf = KNeighborsClassifier(weights='distance')
+    elif s == 'Tree':
+        clf = tree.DecisionTreeClassifier()
+    elif s == 'LinearSVC':
+        clf = make_pipeline(StandardScaler(),LinearSVC(random_state=0, tol=1e-5))
+    elif s == 'SVC':
+        clf = svm.SVC(kernel='linear')
+    return clf
 
-#clf = make_pipeline(StandardScaler(), SGDClassifier(max_iter=1000, tol=1e-3))
-#clf = KNeighborsClassifier(weights='distance')
-clf1 = svm.LinearSVC()
-clf2 = svm.SVC(kernel='linear')
-clf3 = make_pipeline(StandardScaler(),LinearSVC(random_state=0, tol=1e-5))
-clf4 = tree.DecisionTreeClassifier()
+def get_arguments():
+    parser = argparse.ArgumentParser(description='Molecular autoencoder network')
+    parser.add_argument('--model', type=str, default='SDG',
+            help='choose the trained model.please enter: SDG, KNN, Tree, LinearSVC, or SVC')
 
-clf1.fit(train_sets, train_labels)
+    
+    return parser.parse_args()
 
-outputs1 = clf1.predict(train_sets) 
-print("训练集1：", accuracy_score(train_labels,outputs1))
-test_outputs1 = clf1.predict(test_sets) 
-print("测试集1：", accuracy_score(test_labels,test_outputs1))
+def main():
+    
+    for i in range(5):
+        load_data()
+    print('Data load finished.')
 
+    train_sets = data_arr[:int(len(data_arr)*0.9)]
+    test_sets = data_arr[int(len(data_arr)*0.9):]
+    train_labels = label_arr[:int(len(data_arr)*0.9)]
+    test_labels = label_arr[int(len(data_arr)*0.9):]
 
+    args = get_arguments()
+    s=args.model
+    clf = create_model(s)
+    clf.fit(train_sets, train_labels)
+    outputs = clf.predict(train_sets) 
+    print("训练集：", accuracy_score(train_labels,outputs))
 
+    test_outputs = clf.predict(test_sets) 
+    print("测试集：", accuracy_score(test_labels,test_outputs))
+
+if __name__ == "__main__":
+    cur_file_id = 1
+    BoC_size = 0
+    dataset = None
+    data_arr = None
+    label_arr = None
+    main()
