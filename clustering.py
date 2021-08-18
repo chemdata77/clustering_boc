8a7d6679b1043c61f90376564e23f84f0fcca402
zhihuiqin <root@localhost.localdomain>
2021-08-18 11:00:23 +0800

    meanshift

--- clustering.py
+++ clustering.py
@@ -26,7 +26,7 @@ random.seed(seed)
 np.random.seed(seed)
 
 import pickle
-with open("./data/data_dict_mini.lst", 'rb') as fp:
+with open("/data/qzh/julei/data_dict_mini.lst", 'rb') as fp:
     data_dict = pickle.load(fp)
 #print(len(data_dict))
 
@@ -58,53 +58,53 @@ for k in data_dict.keys():
         continue
     # mean-shift
     label_unique = []
-    #bandwidth = estimate_bandwidth(X, quantile=0.3, n_samples=10000, random_state=9)
-    #ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
-    #ms.fit(X)
-    #labels = ms.labels_
-    #label_unique = np.unique(labels)
-    #if(len(label_unique) > 1):
-        #print('MS: ', k, len(label_unique), metrics.silhouette_score(X, labels, sample_size=10000), len(X))
-    #total_clst_cnt += len(label_unique)
-    #clst_dict[k] = ms.cluster_centers_
+    bandwidth = estimate_bandwidth(X, quantile=0.3, n_samples=10000, random_state=9)
+    ms = MeanShift(bandwidth=bandwidth, bin_seeding=True)
+    ms.fit(X)
+    labels = ms.labels_
+    label_unique = np.unique(labels)
+    if(len(label_unique) > 1):
+        print('MS: ', k, len(label_unique), metrics.silhouette_score(X, labels, sample_size=10000), len(X))
+    total_clst_cnt += len(label_unique)
+    clst_dict[k] = ms.cluster_centers_
 
     #mini-batch k means
-    best_score = 0
-    best_clst_cnts = 2
+    #best_score = 0
+    #best_clst_cnts = 2
     #res = []
     for n_clst in range(2, 100):
         if n_clst > 5 and n_clst % 2 == 0:
             continue 
-        km = KMeans(n_clusters=n_clst, random_state=9)
-        y_pred = km.fit_predict(X)
+        #km = KMeans(n_clusters=n_clst, random_state=9)
+        #y_pred = km.fit_predict(X)
 
         #brc = Birch(n_clusters = n_clst, threshold = 0.3, branching_factor = 50)
         #y_pred = brc.fit_predict(X)
 
         # print(metrics.calinski_harabaz_score(X, y_pred))
-        slht = metrics.silhouette_score(X, y_pred, sample_size=1000)
+        #slht = metrics.silhouette_score(X, y_pred, sample_size=1000)
         #res.append([k, slht])
         # slht = metrics.calinski_harabasz_score(X, y_pred)
-        if(slht > best_score):
-            best_score = slht
-            best_clst_cnts = n_clst
-            patience = 0
-        else:
-            patience += 1
-        if patience > 5 and n_clst>10:
-            break
+        #if(slht > best_score):
+            #best_score = slht
+            #best_clst_cnts = n_clst
+            #patience = 0
+        #else:
+            #patience += 1
+        #if patience > 5 and n_clst>10:
+            #break
         #print('res',res)
     #print('KM: ', k, best_clst_cnts, best_score, len(X))
 
     #print('brc: ', k, best_clst_cnts, best_score, len(X))
-    km = KMeans(n_clusters=best_clst_cnts, random_state=9)
-    y_pred = km.fit_predict(X)
+    #km = KMeans(n_clusters=best_clst_cnts, random_state=9)
+    #y_pred = km.fit_predict(X)
     #brc = Birch(n_clusters = best_clst_cnts, threshold = 0.3, branching_factor = 50)
     #y_pred = brc.fit_predict(X)
     #fig_data.append(res)
-    total_clst_cnt += max(best_clst_cnts, len(label_unique))
-    if best_clst_cnts > len(label_unique):
-        clst_dict[k] = km.cluster_centers_
+    #total_clst_cnt += max(best_clst_cnts, len(label_unique))
+    #if best_clst_cnts > len(label_unique):
+        #clst_dict[k] = km.cluster_centers_
         #clst_dict[k] = brc.cluster_centers_
 
     if len(k) == 2:
@@ -114,8 +114,8 @@ for k in data_dict.keys():
        fig_Y = [0 for i in range(4000)]
        for x in X:
            fig_Y[int(x[0]*1000)]+=1
-       non_zero_X = [fig_X[i] for i in range(4000) if fig_Y[i] >= int(max(fig_Y)/20)]
-       non_zero_Y = [fig_Y[i] for i in range(4000) if fig_Y[i] >= int(max(fig_Y)/20)]
+       non_zero_X = [fig_X[i] for i in range(4000)]
+       non_zero_Y = [fig_Y[i] for i in range(4000)]
        ax.set_title(k[0] + '-' + k[1])
        ax.plot(list(np.array(non_zero_X)/1000.0), non_zero_Y)
        # print(results[0][4])
@@ -145,10 +145,10 @@ print(total_clst_cnt)
 
 axs[5].set_ylabel('pair count')
 axs[7].set_xlabel('pair distance, ' + r'$\AA$')
-plt.savefig('./data/kmeans/clst_kmeans_v2.jpg')
+plt.savefig('./data/meanshift/ms.jpg')
 plt.show()
 
 #with open('/data/bak/qzh/e2e_reaction_test/julei_mini/shuju/slht_fig_data_02.txt', 'wb') as fp:
     #pickle.dump(fig_data, fp) 
-with open('./data/kmeans/clst_dict_kmeans_v2.dct', 'wb') as fp:
+with open('./data/meanshift/clst_dict_ms.dct', 'wb') as fp:
     pickle.dump(clst_dict, fp)
