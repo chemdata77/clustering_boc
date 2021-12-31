b892517137686778ecf71c201cd668b5c331728e
zhihuiqin <qzh@bistu.edu.cn>
2021-12-31 09:41:14 +0800

    retest birch_2993

--- clustering_birch.py
+++ clustering_birch.py
@@ -23,7 +23,7 @@ import random
 from yellowbrick.cluster import intercluster_distance
 from yellowbrick.cluster import silhouette_visualizer
 from yellowbrick.cluster.elbow import kelbow_visualizer
-
+import time
 seed = 1234
 random.seed(seed)
 np.random.seed(seed)
@@ -54,33 +54,35 @@ clst_dict = {}
 total_clst_cnt = 0
 
 #fig_data = []
+t0 = time.time()
 for k in data_dict.keys():
     X = data_dict[k]
     if len(X) < 10000:
         continue
     #res = []
-    best_score = 0
-    best_clst_cnts = 2
-    for T in range(1,5):
-        for B in range(40,101):
-            brc = Birch(n_clusters = None,threshold=T/4,branching_factor=B)
-            y_pred = brc.fit_predict(X)
-            if len(set(y_pred)) <= 1:
-                continue
-            slht = metrics.silhouette_score(X, y_pred, sample_size=1000)
-            if(slht > best_score):
-                best_score = slht
-                best_T = T
-                best_B = B
-                patience = 0
-            else:
-                patience += 1
-            if patience > 5:
-                break
-    print(best_T,best_B)
-    brc = Birch(n_clusters = None,threshold=best_T/4,branching_factor=best_B)
+#    best_score = 0
+#    best_clst_cnts = 2
+#    for T in range(1,5):
+#        for B in range(40,101):
+#            brc = Birch(n_clusters = None,threshold=T/4,branching_factor=B)
+#            y_pred = brc.fit_predict(X)
+#            if len(set(y_pred)) <= 1:
+#                continue
+#            slht = metrics.silhouette_score(X, y_pred, sample_size=1000)
+#            if(slht > best_score):
+#                best_score = slht
+#                best_T = T
+#                best_B = B
+#                patience = 0
+#            else:
+#                patience += 1
+#            if patience > 5:
+#                break
+#    print(best_T,best_B)
+    #brc = Birch(n_clusters = None,threshold=best_T/4,branching_factor=best_B)
+    brc = Birch(n_clusters = None)
     y_pred = brc.fit_predict(X) 
-    print(k, best_T,best_B,best_score, len(brc.subcluster_centers_))
+    print(k, metrics.silhouette_score(X, y_pred, sample_size=1000), len(brc.subcluster_centers_))
     #fig_data.append(res)
     total_clst_cnt += len(brc.subcluster_centers_)
     clst_dict[k] = brc.subcluster_centers_
@@ -89,22 +91,22 @@ for k in data_dict.keys():
     #silhouette_visualizer(brc(best_clst_cnts), X, colors='yellowbrick', outpath="./data/birch/sl.png")
     #intercluster_distance(brc(best_clst_cnts), X, outpath="./data/birch/inter.png")
 
-    if len(k) == 2:
-       ax = axs[ax_idx]
-       ax_idx+=1
-       fig_X = [[i] for i in range(4000)]
-       fig_Y = [0 for i in range(4000)]
-       for x in X:
-           fig_Y[int(x[0]*1000)]+=1
-       non_zero_X = [fig_X[i] for i in range(4000)]
-       non_zero_Y = [fig_Y[i] for i in range(4000)]
-       ax.set_title(k[0] + '-' + k[1])
-       ax.plot(list(np.array(non_zero_X)/1000.0), non_zero_Y)
-       # print(results[0][4])
-       for i in range(len(clst_dict[k])):
-           #plt.axvline(x=clst_dict[0][i][0],ls="-",c="green")#添加垂直直线
-           ax.plot([clst_dict[k][i][0], clst_dict[k][i][0]], [0, max(non_zero_Y)], linestyle=':')
-       
+#    if len(k) == 2:
+#       ax = axs[ax_idx]
+#       ax_idx+=1
+#       fig_X = [[i] for i in range(4000)]
+#       fig_Y = [0 for i in range(4000)]
+#       for x in X:
+#           fig_Y[int(x[0]*1000)]+=1
+#       non_zero_X = [fig_X[i] for i in range(4000)]
+#       non_zero_Y = [fig_Y[i] for i in range(4000)]
+#       ax.set_title(k[0] + '-' + k[1])
+#       ax.plot(list(np.array(non_zero_X)/1000.0), non_zero_Y)
+#       # print(results[0][4])
+#       for i in range(len(clst_dict[k])):
+#           #plt.axvline(x=clst_dict[0][i][0],ls="-",c="green")#添加垂直直线
+#           ax.plot([clst_dict[k][i][0], clst_dict[k][i][0]], [0, max(non_zero_Y)], linestyle=':')
+#       
        
 
     #DBSCAN
@@ -124,13 +126,12 @@ for k in data_dict.keys():
     
 
 print(total_clst_cnt)
-
-axs[5].set_ylabel('pair count')
-axs[7].set_xlabel('pair distance, ' + r'$\AA$')
-plt.savefig('/data2/qzh/data_3.5/birch_bestT/birch.jpg')
-plt.show()
-
-#with open('/data/bak/qzh/e2e_reaction_test/julei_mini/shuju/slht_fig_data_02.txt', 'wb') as fp:
-    #pickle.dump(fig_data, fp) 
-with open('/data2/qzh/data_3.5/birch_0.25/clst_dict_birch.dct', 'wb') as fp:
-    pickle.dump(clst_dict, fp)
+t1 = time.time()
+print("seconds wall time",t1-t0)
+#axs[5].set_ylabel('pair count')
+#axs[7].set_xlabel('pair distance, ' + r'$\AA$')
+#plt.savefig('/data2/qzh/data_3.5/birch_test/birch.jpg')
+#plt.show()
+#
+#with open('/data2/qzh/data_3.5/birch_test/clst_dict_birch.dct', 'wb') as fp:
+#    pickle.dump(clst_dict, fp)
